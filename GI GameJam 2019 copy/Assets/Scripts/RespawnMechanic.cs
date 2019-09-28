b01a79526272ef0cc13074d814f3487af57ffded
Elaine <elainezhong@v1040-wn-rt-a-112-92.campus-dynamic.uwaterloo.ca>
2019-09-28 18:01:18 -0400

    DIE

---
+++ GI GameJam 2019 copy/Assets/Scripts/RespawnMechanic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnMechanic : MonoBehaviour
+{
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y <= -20)
+        {
+            this.transform.position = startPosition;
+        }
+    }
+}

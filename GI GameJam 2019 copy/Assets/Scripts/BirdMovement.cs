f4f0a91abc2fc2c83f41c5ea545ff9eea7c0e226
Elaine <elainezhong@v1040-wn-rt-a-112-92.campus-dynamic.uwaterloo.ca>
2019-09-28 17:54:00 -0400

    Update BirdMovement.cs

--- GI GameJam 2019 copy/Assets/Scripts/BirdMovement.cs
+++ GI GameJam 2019 copy/Assets/Scripts/BirdMovement.cs
@@ -12,6 +12,7 @@ public class BirdMovement : MonoBehaviour
 	public AudioSource audioSource;
 	public Transform fireTransform;
 	public float BurnDuration = 15f;
+    //public Camera cam;
 
 	private KeyCode[] upKey = {KeyCode.W, KeyCode.UpArrow};
 	private KeyCode[] downKey = {KeyCode.S, KeyCode.DownArrow};
@@ -84,6 +85,10 @@ public class BirdMovement : MonoBehaviour
 		if (playerIndex == 0 && collision.gameObject.tag == "Fire") {
 			SetLit(true);
 		}
+        //if (playerIndex == 1 && collision.gameObject.tag == "Heavy")
+        //{
+        //    CameraShake(cam);
+        //}
 	}
 
 	void SetLit(bool lit) {
@@ -93,6 +98,11 @@ public class BirdMovement : MonoBehaviour
 			this.burnTimer = BurnDuration;
 		}
 	}
+
+   // void CameraShake(Camera cam)
+   // {
+   //     cam.transform.position = cam.transform.position + new Vector3(0f, Random.Range(0.4f,1f), 0f);
+   // }
 
     //Movement Functions
     void MoveUp(Rigidbody rb)

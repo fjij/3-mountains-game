4bd5519e09c54bd44a8a718c3a36b35443832a78
Elaine <elainezhong@v1041-wn-rt-d-149-174.campus-dynamic.uwaterloo.ca>
2019-09-28 21:48:21 -0400

    UpdateCameraTracker

--- GI GameJam 2019 copy/Assets/Scripts/CameraTracker.cs
+++ GI GameJam 2019 copy/Assets/Scripts/CameraTracker.cs
@@ -19,6 +19,9 @@ public class CameraTracker : MonoBehaviour
     void Update()
     {
         nextPosition = 0.5f*(player.transform.position + player2.transform.position) + offset;
+        nextPosition = new Vector3(0.5f * (player.transform.position.x + player2.transform.position.x),
+            0.5f * (player.transform.position.y + player2.transform.position.y),
+            Mathf.Min(player.transform.position.z, player2.transform.position.z)) + offset;
         this.transform.position = Vector3.Lerp(this.transform.position, nextPosition, Time.deltaTime * movementSpeed);
             //player.transform.position + offset;
         //this.transform.LookAt(player.transform);

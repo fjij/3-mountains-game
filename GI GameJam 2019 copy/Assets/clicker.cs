b80dc900416f99640878eb9a645085369b24c19a
coacoapuff <55902041+coacoapuff@users.noreply.github.com>
2019-09-29 13:10:43 -0400

    yeet

---
+++ GI GameJam 2019 copy/Assets/clicker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class clicker : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource audioz;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        audioz.Play();
+    }
+
+}

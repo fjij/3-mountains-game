2f77e0b616b9272445935c8bd681068d40ccbde2
Mosang-Liu <50381108+Mosang-Liu@users.noreply.github.com>
2019-09-29 15:55:37 -0400

    Add a flag sign to check point

---
+++ GI GameJam 2019 copy/Assets/LookAT.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAT : MonoBehaviour
+{
+    public GameObject flag;
+    public Transform mainCamera;
+    public bool isPuzzleViewOn = false;
+
+    Transform presetTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        presetTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPuzzleViewOn == false)
+        {
+            flag.SetActive(true);
+            transform.LookAt(mainCamera);
+        }
+
+        else if (isPuzzleViewOn)
+            flag.SetActive(false);
+          
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isPuzzleViewOn = !isPuzzleViewOn;
+        }
+    }
+}
